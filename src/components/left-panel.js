ef1bb1d644054631ed24d9c60e99531020b352bf
Naveed Aheer <naveedaheer@gmail.com>
2020-01-23 12:26:45 +0500

    Created left panel component

---
+++ src/components/left-panel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const LeftPanel = () => {
+   return (
+<div style={{color:'#fff', backgroundColor:'#3D63D2', maxWidth:'20%', height:'100vh'}} >
+    <h5>CCM</h5>
+</div>
+    );
+}
+export default LeftPanel;
